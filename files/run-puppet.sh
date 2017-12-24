d694ca80830443a932f707cf4602511548afa790
George <pianan99@hotmail.com>
2017-12-24 11:16:03 +1100

    changes

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /etc/puppetlabs/code/environments/production/puppet && sudo git pull
+cd /etc/puppetlabs/code/environments/production && sudo git pull
 sudo /opt/puppetlabs/bin/puppet apply manifests/
